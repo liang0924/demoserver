b92606cb9a1a04962465466b63ae05970c2a717e
fanhailiang <1275940484@qq.com>
2020-09-06 18:15:49 +0800

    第一次修改

---
+++ src/main/java/com/example/demoserver/Testgit.java
@@ -0,0 +1,14 @@
+package com.example.demoserver;
+
+/**
+ * ClassName:    Testgit
+ * Package:    com.example.demoserver
+ * Description:
+ * Datetime:    2020/9/6   18:13
+ * Author:   fanhailiang
+ */
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
